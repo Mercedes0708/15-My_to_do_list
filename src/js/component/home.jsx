29a4c3ed5d1cc1053af9a86d43c47c83e44917ef
Mercedes0708 <144907375+Mercedes0708@users.noreply.github.com>
2024-01-19 20:51:14 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleKeyPress(e) {    
+    if (e.key === "Enter") {
+      setTodos(todos.concat(inputValue));
+      setInputValue("");
+    } 
+  }
+
+  function handleDelete(i) {
+    const newList = [...todos];
+    newList.splice(i, 1);
+    setTodos(newList);
+  };
+
+  // Agregar el  array ->concat
+  // Borrar el array -> filter
+  // Update -> map
+  return (
+    <div className="container">
+      <h1> My to do list</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={((e) => {setInputValue(e.target.value)})}
+            value={inputValue}
+            onKeyPress={handleKeyPress}
+            placeholder="What do you need to do?"
+          ></input>
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}{" "}
+            <i
+              className="far fa-trash-alt"
+              onClick={handleDelete}
+            ></i>
+          </li>
+        ))}
+      </ul>
+      <div> {todos.length} tasks </div>
+    </div>
+  );
 };
 
-export default Home;
+export default Home;c
